da2a513d9e4694b7ae0055c137169f5b99e67891
Trainex <50549524+tunbk1002@users.noreply.github.com>
2022-02-26 11:35:47 +0700

    update note model and add pitcher (#2)
    Signed-off-by: tulx <lexuantu1002@gmail.com>

--- Assets/Scripts/Models/Notes.cs
+++ Assets/Scripts/Models/Notes.cs
@@ -5,13 +5,34 @@ using System.Threading.Tasks;
 
 namespace Models
 {
+    public class NoteType
+    {
+        private float? _volume;
+
+        public int MidiKey { get; set; }
+        public float Delay { get; set; } = 0;
+        public float? Length { get; set; }
+        public float? Volume 
+        {
+            get => _volume;
+            set 
+            {
+                if (value < 0)
+                    _volume = 0;
+                else if (value > 1)
+                    _volume = 1;
+                else
+                    _volume = value;
+            }
+        }
+    }
+
     public class Note
     {
         // relative to current row [1,2,3,4]
         public int Position { get; set; }
         // [C,D,E,F,G,A,B] implement pitch
-        public char NoteType { get; set; }
-        public bool IsSilent { get; set; } = false;
+        public List<NoteType> NoteType { get; set; }
         public bool TouchOptional { get; set; } = false;
     }
 
