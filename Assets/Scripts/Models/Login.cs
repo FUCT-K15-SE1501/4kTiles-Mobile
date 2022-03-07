6059778d9ba13e4022eebb876171d12697c6485e
Quoc An <67252543+quocan17102000@users.noreply.github.com>
2022-03-07 16:08:09 +0700

    Add Script Login&Register (#9)

---
+++ Assets/Scripts/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [Serializable]
+    public class Login
+    {
+        public string password;
+        public string email;
+    }
+    [Serializable]
+    public class LoginResponse
+    {
+        public string data;
+    }
+}
---
+++ Assets/Scripts/Models/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [Serializable]
+    public class Register
+    {
+        public string userName;
+        public string password;
+        public string email;
+    }
+
+    [Serializable]
+    public class RegisterResponse
+    {
+        public string data;
+    }
+}
