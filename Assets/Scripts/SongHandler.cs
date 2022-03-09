1094b2496b13a0f03e657bd5d466797457f3548b
Quoc An <67252543+quocan17102000@users.noreply.github.com>
2022-03-09 14:44:18 +0700

    AddSongHandler

---
+++ Assets/Scripts/SongHandler.cs
@@ -0,0 +1,39 @@
+using Client;
+using Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SongHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    Text ErrorText;
+
+    public void Load()
+    {
+        StartCoroutine(
+        ClientConstants.API.Get("Library", HttpClientRequest.ConvertToResponseAction<SongResponse>(result =>
+        {
+            if (result.Result.errorCode == -1990)
+            {
+                ErrorText.text = "Song does not exist!";
+                return;
+            }
+            if (result.Result.errorCode == 0)
+            {
+                ErrorText.text = "No Error!";
+                return;
+            }
+            if (!result.IsSuccess)
+            {
+                ErrorText.text = "Load Failed!";
+                return;
+            }
+            Debug.Log(result.Result.data);
+        }))
+        );
+    }
+}
