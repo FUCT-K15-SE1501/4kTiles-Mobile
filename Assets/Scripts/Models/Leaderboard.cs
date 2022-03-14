ccd8029eba42231f3b1675b700f4dda04d217722
wiliam411 <54429463+wiliam411@users.noreply.github.com>
2022-03-14 14:57:36 +0700

    Best score and leaderboard (#18)
    * Update best score
    
    * Leaderboard

---
+++ Assets/Scripts/Models/Leaderboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [Serializable]
+    public class Leaderboard
+    {
+        public int accountId;
+        public string userName;
+        public int bestScore;
+    }
+
+    [Serializable]
+    public class LeaderboardResponse
+    {
+        public List<Leaderboard> data;
+    }
+}
